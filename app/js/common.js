678f7ad316e66618099bbc7051db17f529868aeb
Denis Karakchiev <uokersam@gmail.com>
2019-03-12 17:27:45 +0300

    updading styles

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,6 @@
 (function() {
-
+  let copyrightYear = document.querySelector('.footer__copyright-text-year');
+  let d = new Date();
+  let y = d.getFullYear();
+  copyrightYear.textContent = y;
 })();
