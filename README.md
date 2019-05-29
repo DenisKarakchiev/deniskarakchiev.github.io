381f980d61ef15ec72ee74bd4df0845ae0925cd7
Denis Karakchiev <uokersam@gmail.com>
2019-05-29 17:50:57 +0300

    Simplyfing update

--- README.md
+++
@@ -1,4 +0,0 @@
-# uokersam.github.io
-Website of this repo: uokersam.github.io
-
-run: `gulp`
--- app/js/common.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  let copyrightYear = document.querySelector('.footer__copyright-text-year');
-  let d = new Date();
-  let y = d.getFullYear();
-  copyrightYear.textContent = y;
-})();
